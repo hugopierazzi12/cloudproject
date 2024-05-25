0927099dbfd031fa79dc811d0f9e428930d05d0c
hugopierazzi12 <149684071+hugopierazzi12@users.noreply.github.com>
2024-05-25 17:57:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,7 @@ app = Flask(__name__)
 
 # Configuration de la clé Google Cloud
 json_key = {
-    "type": "service_account",
-    "project_id": "cloudproject-424110",
-    "private_key_id": "cd884a9677b1eb1f20e4b1fe209eff92636fb73e",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDYg+fLrQz0Xm2W\nYaJWa/Yj1tWNPIbIrofefc3eahQsDzJVymELk+VeHmR6ySeE6rOUHt6z7JmTsul9\nrrDVwtSPlYB8K46rsOCsFcJnMwlFkIPW+O2P0ml1KfCl7ydwm2Cf+Zo6MsYBD4yv\nrQiHKfF89rlMBBpbFDntjMdx4LjqhPEzf5B7AXUaUe9zdz3isrxxb6s7hC7oGkTY\nUl0vvMwAsiNx9vc4rFgTpiF3t71SQEv/DIBnojKay14QB74VzzNCzDTxUEHxF/tL\nZB38TUZVyTi51yxVb+fcAWuanWP+cYX8x9yj9q81k4i/2EKt3CeX2i3jr614gnAL\nVBm8oyOJAgMBAAECggEAIBpcw+MehNw8cPv8g0ZiOlmoE9c53+ca77clD3mYkJjX\nPIezoHEXJP+qI9eQE8HuCwQRvslR0yfHvZIpl6RZ+okvAho+PwBMq89VIhKb6RPe\nrID/zl+jMdNcYmolpalwKAAtKTxuhek9ka29Ejd6ni4B9v6zvrXuyeViDCVHHcR+\n/JHWo9M+bi68mo3aAfmiOmQyqB4xd0XL6k7zN1EdytkkEKRLgj6eSxCqXLiCRUxp\npcqdWjn3VDKHOS/dyJe9vw8rsqP2E8Js9iCHPXbG1k7E12y4IKLfUvPFBVKmAT7X\nR5ITCVF39/v2WjmdPiyeO3TcWaw1Fk4y49uPJhLzRQKBgQD7Yyn+ReO/1RFh15Lo\nNavGXgOT9C2ycXUB6Pl1RM9WP+1ryUBTNU0KanuhYBk3HhwJ/SNjW6XLc+jBC2Ga\nyNUtqzZvLrTG64cpxVDVlWiciPeuo8YWwEAeaFfMaG627Deyy7ATEdTLnuSyNWyw\nF+sdlDG7/iZ80tBCd4Nq55Wm1QKBgQDcfO//2cSmJNZB0fdNJrr2yVlRpOEeEGGC\ntpPbzHARi2U53Hwf6zXtfZD4hI8NrJr+6IAofcSM35zjFwWtr1kNWXCKVnAE1tsq\nHsioZXhbqKumyKufQ9jG//Csvc/73XD74/BrL7vp3CSjEi0mMfdgpcTWLXgZOD7D\nMEkUN5LL5QKBgQCzn4ul8HJn4+rjqpGB8remqg6MbXEpjAA7OSjmLiCoVE1lMwwP\naIp/4s4r5Oqfg5gtWv8qQ5YX5d5t8Z/wZYhNdYUTtJ/fcvPFWQQFWRjCoOu5kbQ9\nFWm7UHtLx2M0uVyjGP/a4GbYh9SJsbrTqIOLQxS2a0c88bV1iMgSXx+DcQKBgHSf\n3S5+mIatC2uLTPzRFKm+vPDzfmOxlHJYcoMbctfE3MkrN7iGaGLzPQBG1YgNGXrl\nrgw84f8FtG1l2woQqtDl0yJJMD1PDGQOHmL8MRqCcDDrCeRXNc2kyUAFsoJtkfqa\niauYdxPu7q4Wyize1xOW+zOyn0jvuDr0SmNYNdyFAoGAHcbvQHzUM4vXkPBus6A0\nfumrfatmYCwVKl8zwUA5NgO/cZrdVIeO75RghYZ8M7kcbE92wSrd6wa2tQBFw6EO\nzXK93b3jSVfV+eKYrx7ZpZLLPx0TRv/2QEQhKJ3yCRj681/LF+Tw8+ZvXE51LdtY\nuwg34ehcKtzQy5aQFF64JlU=\n-----END PRIVATE KEY-----\n",
-    "client_email": "cloudproject@cloudproject-424110.iam.gserviceaccount.com",
-    "client_id": "115004387933230655429",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/cloudproject-424110.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
+    XXX
 }
 
 credentials = service_account.Credentials.from_service_account_info(json_key)
@@ -35,7 +25,7 @@ YOUR_HASH_PASSWD = hashlib.sha256(YOUR_PASSWORD.encode()).hexdigest()
 tts_client = texttospeech.TextToSpeechClient(credentials=credentials)
 
 # Clé API OpenAI
-openai.api_key = "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"
+openai.api_key = "XXX"
 
 # Nouvelle méthode pour initialiser le client OpenAI
 client_openai = openai.OpenAI(api_key=openai.api_key)
